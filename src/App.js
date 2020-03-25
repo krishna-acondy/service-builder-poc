086ec11dfd2e4f086a87ef34368c7ae34833756f
Krishna Acondy <kc.acondy@outlook.com>
2020-03-25 22:50:53 +0000

    Set up routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import { Menu, Sidebar } from "semantic-ui-react";
+import "./App.scss";
+import "semantic-ui-css/semantic.min.css";
+import ImportExport from "./pages/ImportExport";
+import Configuration from "./pages/Configuration";
+import Services from "./pages/Services";
+import SidebarItem from "./components/SidebarItem";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export default (
+  <div className="root">
+    <HashRouter>
+      <div className="sidebar">
+        <Sidebar
+          as={Menu}
+          animation="overlay"
+          icon="labeled"
+          inverted
+          vertical
+          visible
+          width="thin"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+          <SidebarItem
+            icon="file"
+            link="/import-export"
+            text="Import / Export"
+          />
+          <SidebarItem
+            icon="settings"
+            link="/configuration"
+            text="Configuration"
+          />
+          <SidebarItem icon="server" link="/services" text="Services" />
+        </Sidebar>
+      </div>
+      <div className="main">
+        <Switch>
+          <Route exact path="/import-export" component={ImportExport} />
+          <Route exact path="/configuration" component={Configuration} />
+          <Route exact path="/services" component={Services} />
+        </Switch>
+        <Redirect from="/" to="/import-export" />
+      </div>
+    </HashRouter>
+  </div>
+);
---
+++ src/components/SidebarItem.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Menu, Icon } from "semantic-ui-react";
+import "./SidebarItem.scss";
+
+const SidebarItem = ({ icon, link, text }) => {
+  return (
+    <Link to={link}>
+      <Menu.Item as="span" className="sidebar-item">
+        <Icon name={icon} />
+        {text}
+      </Menu.Item>
+    </Link>
+  );
+};
+
+export default SidebarItem;
