d54caa035d4b275245cba0425f264d2d5b005b08
Krishna Acondy <kc.acondy@outlook.com>
2020-03-28 18:25:59 +0000

    Add folders

--- src/context/appContext.jsx
+++ src/context/appContext.jsx
@@ -1,14 +1,174 @@
 import React, { createContext, useState, useEffect } from "react";
 
+const json = {
+  appConfig: {
+    name: "Krishna Test",
+    description: "Krishna Test SAS App",
+    author: "Krishna"
+  },
+  sasJsConfig: {
+    serverUrl: "http://sas.analytium.co.uk",
+    port: 7980,
+    pathSAS9: "/SASStoredProcess/do",
+    pathSASViya: "/SASJobExecution",
+    appLoc: "/Public/app",
+    serverType: "SAS9",
+    debug: false
+  },
+  folders: [
+    {
+      name: "common",
+      services: [
+        {
+          name: "appInit",
+          description: "My first Service Maker",
+          response: {
+            areas: [
+              {
+                area: "Adak"
+              },
+              {
+                area: "Adel"
+              }
+            ]
+          }
+        },
+        {
+          name: "appInitTest2",
+          description: "My first Service Maker",
+          response: {
+            areas: [
+              {
+                area: "Adak"
+              },
+              {
+                area: "Adel"
+              }
+            ]
+          }
+        },
+        {
+          name: "appInitTest3",
+          description: "My first Service Maker",
+          response: {
+            areas: [
+              {
+                area: "Adak"
+              },
+              {
+                area: "Adel"
+              }
+            ]
+          }
+        },
+        {
+          name: "appInitTest4",
+          description: "My first Service Maker",
+          response: {
+            areas: [
+              {
+                area: "Adak"
+              },
+              {
+                area: "Adel"
+              }
+            ]
+          }
+        },
+        {
+          name: "appInitTest5",
+          description: "My first Service Maker",
+          response: {
+            areas: [
+              {
+                area: "Adak"
+              },
+              {
+                area: "Adel"
+              }
+            ]
+          }
+        },
+        {
+          name: "getData",
+          description: "My Another Service Maker",
+          request: {
+            areas: [
+              {
+                area: "Adel"
+              }
+            ]
+          },
+          response: {
+            springs: [
+              {
+                area: "Adak",
+                s: "spring1",
+                "-": "44"
+              },
+              {
+                area: "Adel",
+                s: "spring2",
+                "-": "22"
+              },
+              {
+                area: "blah",
+                s: "sprin3",
+                "-": "-3"
+              }
+            ]
+          }
+        }
+      ]
+    },
+    {
+      name: "admin",
+      services: [
+        {
+          name: "createUser",
+          description: "creating a new user (admin task)",
+          response: {
+            newuser: [
+              {
+                name: "Krishna",
+                id: 42,
+                I: "IE"
+              }
+            ]
+          }
+        },
+        {
+          name: "getData",
+          description: "My Another Service Maker",
+          request: {
+            deleteuser: [
+              {
+                username: "johnny"
+              }
+            ]
+          },
+          response: {
+            result: [
+              {
+                success: "true"
+              }
+            ]
+          }
+        }
+      ]
+    }
+  ]
+};
+
 export const AppContext = createContext({
-  masterJson: null,
+  masterJson: json,
   setMasterJson: json => {},
   isNewJson: false,
   setIsNewJson: value => {}
 });
 
 export const AppProvider = ({ children }) => {
-  const [masterJson, setMasterJson] = useState(null);
+  const [masterJson, setMasterJson] = useState(json);
   const [isNewJson, setIsNewJson] = useState(false);
 
   useEffect(() => {
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -5,17 +5,22 @@ import {
   Icon,
   Message,
   Card,
+  Popup,
+  Modal,
   Button,
-  Confirm
+  Input,
+  Form
 } from "semantic-ui-react";
 import "./Services.scss";
 import { AppContext } from "../context/appContext";
+import Folder from "../components/Folder";
 
 const Services = () => {
   const { masterJson, setMasterJson } = useContext(AppContext);
   const folders = masterJson && masterJson.folders ? masterJson.folders : [];
   const [currentFolder, setCurrentFolder] = useState(null);
-  const [showConfirmDelete, setShowConfirmDelete] = useState(false);
+  const [addFolderModalOpen, setaddFolderModalOpen] = useState(false);
+  const [showFolderNameError, setShowFolderNameError] = useState(false);
 
   const deleteFolder = folder => {
     const newFolders = folders.filter(f => f.name !== folder.name);
@@ -28,50 +33,34 @@ const Services = () => {
       <Header as="h1">Services</Header>
       <div className="main-content">
         <Segment raised size="huge" className="folders">
-          <Header as="h3">Folders</Header>
-          <Button icon>
-            <Icon name="add" />
-            Add folder
-          </Button>
+          <h3>Folders</h3>
+          <Popup
+            inverted
+            content="Add folder"
+            trigger={
+              <Icon
+                name="add"
+                color="blue"
+                onClick={() => setaddFolderModalOpen(true)}
+              />
+            }
+          />
           <div className="folder-list">
             {folders.map((folder, index) => {
               return (
-                <div key={index} className="folder-container">
-                  <div
-                    className={
-                      currentFolder === folder
-                        ? "folder folder-selected"
-                        : "folder"
-                    }
-                    onClick={() => setCurrentFolder(folder)}
-                  >
-                    <Icon name="folder"></Icon> {folder.name}
-                  </div>
-                  <Icon
-                    name="trash alternate outline"
-                    color="red"
-                    onClick={() => {
-                      setShowConfirmDelete(true);
-                      setCurrentFolder(folder);
-                    }}
-                  />
-                </div>
+                <Folder
+                  key={index}
+                  folder={folder}
+                  selected={currentFolder === folder}
+                  onClick={() => setCurrentFolder(folder)}
+                  onDelete={deleteFolder}
+                />
               );
             })}
-            {currentFolder && (
-              <Confirm
-                open={showConfirmDelete}
-                header={`Delete Folder ${currentFolder.name}`}
-                content="Are you sure you want to delete this folder?"
-                confirmButton="Yes"
-                onCancel={() => setShowConfirmDelete(false)}
-                onConfirm={() => deleteFolder(currentFolder)}
-              />
-            )}
           </div>
         </Segment>
         <Segment raised size="large" className="services">
-          <Header as="h3">Services</Header>
+          <h3>Services</h3>
           {!currentFolder && (
             <Message info>
               <Message.Header>No folder selected</Message.Header>
@@ -92,6 +81,61 @@ const Services = () => {
           )}
         </Segment>
       </div>
+      <Modal open={addFolderModalOpen} size="tiny">
+        <Header icon="add" content="Add New Folder" />
+        <Form
+          className="new-folder-form"
+          onSubmit={e => {
+            const newFolderName = e.target.elements.folderName.value;
+            const folderExists = folders.some(f => f.name === newFolderName);
+            if (!folderExists) {
+              const newFolder = { name: newFolderName, services: [] };
+              setMasterJson({
+                ...masterJson,
+                folders: [...masterJson.folders, newFolder]
+              });
+              setCurrentFolder(newFolder);
+              setaddFolderModalOpen(false);
+            } else {
+              setShowFolderNameError(true);
+            }
+          }}
+        >
+          <Modal.Content>
+            {showFolderNameError && (
+              <Message negative>
+                <Message.Header>
+                  A folder with that name already exists
+                </Message.Header>
+                Please try again with another folder name.
+              </Message>
+            )}
+            <h4>Enter the name for your new folder.</h4>
+            <Input
+              type="text"
+              name="folderName"
+              placeholder="Folder Name"
+              maxLength="32"
+              pattern="[_a-zA-Z][_a-zA-Z0-9]*"
+              required
+            />
+            <div className="naming-conventions">
+              Please make sure to follow the SAS naming convention for names:
+              <ul>
+                <li>Starts with a letter or an underscore.</li>
+                <li>Contains only letters, numbers and underscores.</li>
+                <li>Has a maximum length of 32.</li>
+              </ul>
+            </div>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button onClick={() => setaddFolderModalOpen(false)}>Cancel</Button>
+            <Button type="submit" color="green">
+              Add
+            </Button>
+          </Modal.Actions>
+        </Form>
+      </Modal>
     </div>
   );
 };
