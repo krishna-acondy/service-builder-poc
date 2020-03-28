7487cf0e9504971ec7707005c6f1f4a0e34b8184
Krishna Acondy <kc.acondy@outlook.com>
2020-03-28 18:43:21 +0000

    Extract AddFolderModal into separate component

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,19 +1,9 @@
 import React, { useContext, useState } from "react";
-import {
-  Header,
-  Segment,
-  Icon,
-  Message,
-  Card,
-  Popup,
-  Modal,
-  Button,
-  Input,
-  Form
-} from "semantic-ui-react";
+import { Header, Segment, Icon, Message, Card, Popup } from "semantic-ui-react";
 import "./Services.scss";
 import { AppContext } from "../context/appContext";
 import Folder from "../components/Folder";
+import AddFolderModal from "../components/AddFolderModal";
 
 const Services = () => {
   const { masterJson, setMasterJson } = useContext(AppContext);
@@ -81,61 +71,25 @@ const Services = () => {
           )}
         </Segment>
       </div>
-      <Modal open={addFolderModalOpen} size="tiny">
-        <Header icon="add" content="Add New Folder" />
-        <Form
-          className="new-folder-form"
-          onSubmit={e => {
-            const newFolderName = e.target.elements.folderName.value;
-            const folderExists = folders.some(f => f.name === newFolderName);
-            if (!folderExists) {
-              const newFolder = { name: newFolderName, services: [] };
-              setMasterJson({
-                ...masterJson,
-                folders: [...masterJson.folders, newFolder]
-              });
-              setCurrentFolder(newFolder);
-              setaddFolderModalOpen(false);
-            } else {
-              setShowFolderNameError(true);
-            }
-          }}
-        >
-          <Modal.Content>
-            {showFolderNameError && (
-              <Message negative>
-                <Message.Header>
-                  A folder with that name already exists
-                </Message.Header>
-                Please try again with another folder name.
-              </Message>
-            )}
-            <h4>Enter the name for your new folder.</h4>
-            <Input
-              type="text"
-              name="folderName"
-              placeholder="Folder Name"
-              maxLength="32"
-              pattern="[_a-zA-Z][_a-zA-Z0-9]*"
-              required
-            />
-            <div className="naming-conventions">
-              Please make sure to follow the SAS naming convention for names:
-              <ul>
-                <li>Starts with a letter or an underscore.</li>
-                <li>Contains only letters, numbers and underscores.</li>
-                <li>Has a maximum length of 32.</li>
-              </ul>
-            </div>
-          </Modal.Content>
-          <Modal.Actions>
-            <Button onClick={() => setaddFolderModalOpen(false)}>Cancel</Button>
-            <Button type="submit" color="green">
-              Add
-            </Button>
-          </Modal.Actions>
-        </Form>
-      </Modal>
+      <AddFolderModal
+        isOpen={addFolderModalOpen}
+        showFolderNameError={showFolderNameError}
+        onCancel={() => setaddFolderModalOpen(false)}
+        onAddFolder={newFolderName => {
+          const folderExists = folders.some(f => f.name === newFolderName);
+          if (!folderExists) {
+            const newFolder = { name: newFolderName, services: [] };
+            setMasterJson({
+              ...masterJson,
+              folders: [...masterJson.folders, newFolder]
+            });
+            setCurrentFolder(newFolder);
+            setaddFolderModalOpen(false);
+          } else {
+            setShowFolderNameError(true);
+          }
+        }}
+      />
     </div>
   );
 };
